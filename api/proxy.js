eab237208c55bbc73a76ad8b969fc0404f5b0f45
pbarakat-a11y <pbarakat@servicetitan.com>
2026-05-14 14:45:47 -0700

    Delete api directory

--- api/proxy.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET");
-
-  const url = "https://script.google.com/macros/s/AKfycbzRyuIF5rtToW4vysM65NAjcBUBDLBv2Poxmb6FYUHV_aurkdCIgiryN1DQRDftwl1P/exec";
-  const response = await fetch(url, { redirect: "follow" });
-  const data = await response.json();
-  res.status(200).json(data);
-}
